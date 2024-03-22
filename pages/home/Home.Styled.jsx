cafbf4f5c1ec7fa058a9792a61bdcc0729bad482
parkjise <monories79@gmail.com>
2024-03-22 11:48:05 +0900

    first

---
+++ pages/home/Home.Styled.jsx
@@ -0,0 +1,17 @@
+import styled from "styled-components"
+
+export const StyledHome = styled.div`
+    background-color: red;
+    width: 100vw;
+    height: 100vh;
+  `
+  const HomePage1 = styled("div")`
+    background-color: blue;
+    width: 100vw;
+    height: 100vh;
+  `
+  const HomePage2 = styled.div({
+    backgroundColor: "purple",
+    width: "100vw",
+    height: "100vh"
+  })
---
+++ pages/home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyledHome } from './Home.Styled'
+
+
+const Home = () => {
+  
+  return (
+    <StyledHome>
+      Home
+    </StyledHome>
+  )
+}
+
+export default Home
